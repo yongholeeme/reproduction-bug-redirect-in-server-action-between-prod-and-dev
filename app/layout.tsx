774903eb56600dae8f062d05b92ebcee672cb9cf
yongholeeme <yongholeeme@gmail.com>
2025-03-12 17:08:36 +0900

    Add mutiple navigation ways

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex flex-col items-center justify-center min-h-screen gap-4">
+          <h1 className="text-neutral-400">reproduction-bug-redirect-in-server-action-between-prod-and-dev</h1>
+          {children}
+        </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 'use client';
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { move } from "./_action";
+
 export default function Home() {
+  const router = useRouter()
+  
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-      <h1>reproduction-bug-redirect-in-server-action-between-prod-and-dev</h1>
-    </div>
+    <>
+      <Link href="/test">{'<Link />'} to /test</Link>
+      <button onClick={() => {router.push('/test')}}>router.push to /test</button>
+      <button onClick={move}>redirect in server action to /test</button>
+    </>
   );
 }
